2466b6cec1cb78942f1b201d8a3ca7349c2cbee9
CapelleGab <gabin.capelle.pro@gmail.com>
2025-06-10 21:22:38 +0200

    add java bubble_sort

---
+++ algorithms/sorting/bubble_sort/java/bubble_sort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class BubbleSort {
+  public static void main(String[] args) {
+    int[] data = {123, 456, 789, 234, 567, 890, 345, 678, 901, 432, 765, 198, 543, 876, 321, 654, 987, 234, 567, 890, 12, 45, 78, 23, 56, 89, 34, 67, 90, 43, 76, 19, 54, 87, 32, 65, 23, 56, 89, 12, 99, 0, 99};
+
+    int[]sortedList = bubbleSort(data);
+
+    Arrays.stream(sortedList).forEach(System.out::println);
+  }
+
+  public static int[] bubbleSort(int[] data) {
+    int length = data.length;
+
+    for (int i = 0; i < length; i++) {
+      for (int j = 0; j < length - i - 1; j++) {
+        if (data[j] > data[j + 1]) {
+          int temp = data[j];
+          data[j] = data[j + 1];
+          data[j + 1] = temp;
+        }
+      }
+    }
+    return data;
+  }
+}
